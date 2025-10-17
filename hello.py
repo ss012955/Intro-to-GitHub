22bb12d07439c9379a689d65c86d46d944998800
ss012955 <ssicat.k12045955@umak.edu.ph>
2025-10-18 00:44:21 +0800

    fixed the infinite loop bug

--- hello.py
+++ hello.py
@@ -2,10 +2,13 @@ print("Hello Justine Nabunturan Infinity Castle Loop")
 
 x = [3, 5, 7, 9]
 
-while True:
+count = 0  
+
+while count < 5:  
     x.append(7)
     x = [i + 2 for i in x]
     print(x)
+    count += 1  
 
 
 
